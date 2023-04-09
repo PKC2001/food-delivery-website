5b06125e8cde11aa2110cf2d55203863b5b1ca49
PKC2001 <chillawarpranav@gmail.com>
2023-04-09 16:32:38 +0530

    frontend

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,112 @@
+import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/res-logo.png";
+
+import "../../styles/footer.css";
+
+import { Link } from "react-router-dom";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3" md="4" sm="6">
+            <div className=" footer__logo text-start">
+              <img src={logo} alt="logo" />
+              <h5>Tasty Treat</h5>
+              <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
+              Distinctio quasi qui minus quos sit perspiciatis inventore quis provident placeat fugiat!
+              </p>
+            </div>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Delivery Time</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>All Days</span>
+                <p>9:00am - 10:00pm</p>
+              </ListGroupItem>
+
+              {/* <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Friday - Saturday</span>
+                <p>Off day</p>
+              </ListGroupItem> */}
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Contact</h5>
+            <ListGroup className="deliver__time-list">
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <p>Location: Pune</p>
+              </ListGroupItem>
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Phone: 7350243344</span>
+              </ListGroupItem>
+
+              <ListGroupItem className=" delivery__time-item border-0 ps-0">
+                <span>Email: chillawarpranav@gmail.com</span>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+
+          <Col lg="3" md="4" sm="6">
+            <h5 className="footer__title">Newsletter</h5>
+            <p>Subscribe our newsletter</p>
+            <div className="newsletter">
+              <input type="email" placeholder="Enter your email" />
+              <span>
+                <i class="ri-send-plane-line"></i>
+              </span>
+            </div>
+          </Col>
+        </Row>
+
+        <Row className="mt-5">
+          <Col lg="6" md="6">
+            <p className="copyright__text">
+              Copyright - 2023 All Rights
+              Reserved.
+            </p>
+          </Col>
+          <Col lg="6" md="6">
+            <div className="social__links d-flex align-items-center gap-4 justify-content-end">
+              <p className="m-0">Follow: </p>
+              <span>
+                {" "}
+                <Link to="https://instagram.com/pranav_chillawar?igshid=YmMyMTA2M2Y=">
+                  <i class="ri-instagram-line"></i>
+                </Link>{" "}
+              </span>
+
+              <span>
+                <Link to="https://github.com/PKC2001">
+                  <i class="ri-github-line"></i>
+                </Link>
+              </span>
+{/* 
+              <span>
+                {" "}
+                <Link to="https://www.youtube.com/c/MuhibsTechDiary">
+                  <i class="ri-youtube-line"></i>
+                </Link>{" "}
+              </span> */}
+
+              <span>
+                {" "}
+                <Link to="https://www.linkedin.com/in/pranav-chillawar-04078b1ab/">
+                  <i class="ri-linkedin-line"></i>
+                </Link>{" "}
+              </span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
+
+export default Footer;
